a2dca308472b2c00b72243d7132946b815c91c2f
Philip Crowther <crowtherpb@gmail.com>
2022-12-24 12:48:10 -0500

    feat(main): program takes url and downloads first image

--- image-scraper.py
+++ image-scraper.py
@@ -1,5 +1,23 @@
+from bs4 import BeautifulSoup
+import lxml
+import argparse
+import urllib.request
+import shutil
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str)
+    args = parser.parse_args()
+    with urllib.request.urlopen(args.url) as response:
+        page = response.read()
+        soup = BeautifulSoup(page, "lxml")
+        images = soup.find_all('img')
+        image = images[0]
+        url = image.attrs['src']
+        r = urllib.request.urlretrieve(url, 'filename.jpg')
+
+        
+            
 
 if __name__ == "__main__":
     main()
