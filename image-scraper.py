0a3b153a0dd8635ca330737fd1ed4604bb76b6a8
Philip Crowther <crowtherpb@gmail.com>
2022-12-26 23:53:11 -0500

    feat(all images): now gets all images on site

--- image-scraper.py
+++ image-scraper.py
@@ -5,6 +5,7 @@ import urllib.request
 import os
 
 def main():
+
     # initialize argument parser
     parser = argparse.ArgumentParser()
     # add arguments
@@ -12,27 +13,39 @@ def main():
     parser.add_argument('url', type=str)
     # parse arguments
     args = parser.parse_args()
+
     # make HTTP request and perform main operations
     with urllib.request.urlopen(args.url) as response:
+
         # read HTML
         page = response.read()
         # parse HTML
         soup = BeautifulSoup(page, 'lxml')
         # find all images on page
         images = soup.find_all('img')
-        # temp/testing: place image info into variable
-        image = images[0]
-        # place image url into variable
-        url = image.attrs['src']
-        # make image request/download image
-        if not os.path.isdir(args.directory):
-            # return error message if invalid path is given
-            print("Error: No such directory")
-            return
-        # prep file name/location
-        location = os.path.join(args.directory,'filename.jpg')
-        # make request
-        r = urllib.request.urlretrieve(url, location)
+
+
+        #cycle through and retrieve all images 
+        for i in range(len(images)):
+
+            # temp/testing: place image info into variable
+            image = images[i]
+            # check image for source: if it isn't, then go to next image
+            if 'src' not in image.attrs:
+                continue
+            # place image url into variable
+            url = image.attrs['src']
+
+            # check if directory exists
+            if not os.path.isdir(args.directory):
+                # return error message if invalid path is given
+                print("Error: No such directory")
+                return
+
+            # prep file name/location
+            location = os.path.join(args.directory, str(i) + '.jpg')
+            # make request
+            r = urllib.request.urlretrieve(url, location)
 
         
 if __name__ == '__main__':
