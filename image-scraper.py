d1cd443cd523e01f0b2999827d82bb2e6cc6539d
Philip Crowther <crowtherpb@gmail.com>
2022-12-26 19:37:18 -0500

    feat(file path): add argument for file path

--- image-scraper.py
+++ image-scraper.py
@@ -2,12 +2,13 @@ from bs4 import BeautifulSoup
 import lxml
 import argparse
 import urllib.request
-import shutil
+import os
 
 def main():
     # initialize argument parser
     parser = argparse.ArgumentParser()
     # add arguments
+    parser.add_argument('directory', type=str)
     parser.add_argument('url', type=str)
     # parse arguments
     args = parser.parse_args()
@@ -16,7 +17,7 @@ def main():
         # read HTML
         page = response.read()
         # parse HTML
-        soup = BeautifulSoup(page, "lxml")
+        soup = BeautifulSoup(page, 'lxml')
         # find all images on page
         images = soup.find_all('img')
         # temp/testing: place image info into variable
@@ -24,10 +25,15 @@ def main():
         # place image url into variable
         url = image.attrs['src']
         # make image request/download image
-        r = urllib.request.urlretrieve(url, 'filename.jpg')
+        if not os.path.isdir(args.directory):
+            # return error message if invalid path is given
+            print("Error: No such directory")
+            return
+        # prep file name/location
+        location = os.path.join(args.directory,'filename.jpg')
+        # make request
+        r = urllib.request.urlretrieve(url, location)
 
         
-            
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
